8d1f10ae349983b4ddb478d2f1abfdeaf2a749b8
rossssson <809650482@qq.com>
2022-01-28 09:56:08 +0800

    complete section 2.1

--- ch2/Ch2.cpp
+++ ch2/Ch2.cpp
@@ -14,4 +14,28 @@ public:
         show_bytes showBytes;
         showBytes.show((byte_pointer)s,strlen(s));
     }
+    /*use xor to swap*/
+    void swapByXOR(int *a,int *b) {
+        *b = *a ^ *b;
+        *a = *a ^ *b;
+        *b = *a ^ *b;
+    }
+    void func2_11(int a[],int cnt){
+        int first,last;
+        for ( first = 0,last=cnt-1; first <=last/*first<last*/ ; first++,last--) {
+            swapByXOR(&a[first],&a[last]);
+        }
+        //in func2_11, when cnt=2k+1,a[k] will be set to 0 as a[k]^a[k]=0,replace first<=last with first<last to solve
+    }
+    void func2_13(int x,int y)
+    {
+        //bis and bic are instructions used in VAX computer to replace boolean instructions. Both instructions take a data
+        //word x and a mask word m, generating a result z. With bis, x is modified by setting z to 1 at each bit position
+        //where m=1, with bic, z is set to 0 where m=0.
+        //use bis(int x,int y) and bic(int x,int y) to implement x|y and x^y
+
+        /*Clearly, bis(x,y) refers to x|y and bic(x,y) refers to x&y*/
+        /*So, x|y can be implemented by bis(x,y), and x^y can be implemented by bic(bis(x,y),bic(x,y))*/
+
+    }
 };
--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 
 int main() {
     ch2_1 test;
-    show_bytes showBytes;
+    /*show_bytes showBytes;
     int a=123456;
     showBytes.test_show(a);//40e20100 0020f147 f8fd620000000000
-    test.func2_7();//61626364656667
+    test.func2_7();//61626364656667*/
+    /*swap*/
+    /*int m=10,n=15;
+    int*p=&m,*q=&n;
+    test.swapByXOR(p,q);
+    std::cout<<m<<n;//15,10*/
     return 0;
 }
